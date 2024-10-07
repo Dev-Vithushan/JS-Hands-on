55b782d8b3972229facd8fbf792aba48e286f201
Abinaya-Indran <abinayaofficial28@gmail.com>
2024-10-07 10:35:20 +0530

    final basic  added

---
+++ Basic - questions/3.convert.js
@@ -0,0 +1,4 @@
+let name = "1234"
+let number = Number(name);
+console.log(number)
+console.log(typeof(number))
---
+++ Basic - questions/8.larger.js
@@ -0,0 +1,11 @@
+let num1 = 40 ;
+let num2 = 20 ;
+if (num1 > num2) {
+    console.log('num1 larger than num2')
+}
+else if (num2 > num1)  {
+    console.log('num2 larger than num1')
+}
+else {
+    console.log ('both are equal')
+}
