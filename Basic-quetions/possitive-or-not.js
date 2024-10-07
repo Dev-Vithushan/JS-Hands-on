b3307dee3ea12a999aea9d66c77ff4d61df2962b
Thanathipan <thana.yy44@gmail.com>
2024-10-07 10:04:34 +0530

    add4

---
+++ Basic-quetions/possitive-or-not.js
@@ -0,0 +1,8 @@
+// 5. Create a program that checks if a number is positive, negative, or zero.
+        let (num)=-1;
+        if (num>0)
+        console.log("Number is possitive")
+        else if(num<0)
+        console.log("Number is Nagative")
+        else
+        console.log("Ita a zero")
