e1a96f06a07334398fae60ba185b8a65319fa2b9
Abarna Selvanathan <rnaaba21@gmail.com>
2024-10-07 09:45:28 +0530

    added js fill

---
+++ Basic-Question/convertString.js
@@ -0,0 +1,7 @@
+let string="2127"	
+{
+    console.log("is divisible by 3")
+}
+let number=2127
+console.log(typeof( string))
+console.log(typeof( number))
---
+++ Basic-Question/tryReassiging.js
@@ -0,0 +1,3 @@
+const weekdays=7
+weekdays=5
+console.log(weekdays)
