e31957ac5c64a95562cbca52e4d40fe5702fecf0
Thanathipan <thana.yy44@gmail.com>
2024-10-07 09:51:12 +0530

    add3

---
+++ Basic-quetions/string-to-number.js
@@ -0,0 +1,6 @@
+// 3. Write a program to convert a string to a number using JavaScript.
+
+let str ="444"
+let (num) = Number(str)
+console.log(num)
+console.log(typeof num)
--- text.md
+++
@@ -1,3 +0,0 @@
-2. Create a constant variable and try reassigning its value. What happens?
-
-        *The value is not changeable in const is a constant variable
