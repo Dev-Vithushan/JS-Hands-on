6a5e9f1054f5467af2165110c104d2f672264ba3
Abinaya-Indran <abinayaofficial28@gmail.com>
2024-10-07 23:11:36 +0530

    functions  added

---
+++ intermediate/function/2.prime.js
@@ -0,0 +1,11 @@
+function checkprime (num ) {
+    if (num > 1 && num % num !== 0){
+        return primeNumber;
+    }
+ 
+    else{
+        return isnotprimenumber;
+    }
+}
+console.log (checkprime(5));
+    
